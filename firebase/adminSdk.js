4977c1138c5ec3268de99cc4c95b6159189ab221
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-02-16 19:35:46 +0800

    change credentials

--- firebase/adminSdk.js
+++ firebase/adminSdk.js
@@ -1,6 +1,9 @@
 const admin = require("firebase-admin");
 const path = require("path");
-const serviceAccount = path.join(__dirname, "../credentials.json");
+const serviceAccount = path.join(
+  __dirname,
+  "../nwa-bbgdc-firebase-adminsdk-pnq5d-b6d4b97e1a.json"
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { setAdmin, addPatroller } = require("../controller/adminController");
 const { verifyToken } = require("../middleware/requireAuth");
 
-// router.use(verifyToken);
+router.use(verifyToken);
 
 router.get("/custom_claim", setAdmin);
 router.post("/add_patroller", addPatroller);
