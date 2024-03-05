876a5ac16857fc426b93af41ad901c46effe6cae
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-03-05 17:59:04 +0800

    renamed services account file

--- firebase/adminSdk.js
+++ firebase/adminSdk.js
@@ -1,9 +1,6 @@
 const admin = require("firebase-admin");
 const path = require("path");
-const serviceAccount = path.join(
-  __dirname,
-  "../nwa-bbgdc-firebase-adminsdk-pnq5d-b6d4b97e1a.json"
-);
+const serviceAccount = path.join(__dirname, "../credentials.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
